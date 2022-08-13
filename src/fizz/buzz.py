5254377b162b23771f9e06d94a555a221fc9a830
Shinya Yajima <y.megane88@gmail.com>
2022-08-13 13:05:42 +0900

    add sample src

---
+++ src/fizz/buzz.py
@@ -0,0 +1,8 @@
+def fizzbuzz(num: int) -> str:
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    return str(num)
---
+++ src/main.py
@@ -0,0 +1,14 @@
+from logging import getLogger
+import os
+
+from fizz.buzz import fizzbuzz
+
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+logging = getLogger(__name__)
+logging.setLevel(LOG_LEVEL)
+
+if __name__ == "__main__":
+    for i in range(1, 15):
+        ans = fizzbuzz(i)
+        logging.info(f"{i} -> {ans}")
