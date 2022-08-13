3d2419415d91d04d32dd74c942b6bd4b683469a7
Shinya Yajima <y.megane88@gmail.com>
2022-08-13 13:08:56 +0900

    add unit test

---
+++ test/unit/test_fizzbuzz.py
@@ -0,0 +1,11 @@
+import pytest
+
+from fizz.buzz import fizzbuzz
+
+
+@pytest.mark.unit
+def test_fizzbuzz():
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
